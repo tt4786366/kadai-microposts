4dc47747988faabc1ee8aadc6cd58f34e77fe103
tt <tt4786366@gmail.com>
2022-05-18 17:27:31 +0000

    post

--- app/User.php
+++ app/User.php
@@ -36,4 +36,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+
+    /**
+     * このユーザに関係するモデルの件数をロードする。
+     */
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }    
+    
+    
 }
